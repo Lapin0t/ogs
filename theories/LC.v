5d35e88618677cd78a2076a63d6d4571c18d3e61
Lapinot <peio.borthelle@ens-lyon.fr>
2021-03-30 17:25:59 +0200

    eval from whn iteration

--- theories/LC.v
+++ theories/LC.v
@@ -74,7 +74,6 @@ Definition wrap {E X} (t : [ E ]e (itree E X)) : itree E X :=
 Instance Functor_E {E} : Functor ([ E ]e) := {|
   fmap _ _ f := fun x => EF (eF_q x) (f # eF_k x)
 |}. 
-Search "Functor".
 
 Instance Functor_E_itree {E} : Functor ([ E ]e # itree E) := {|
   fmap _ _ f := fun x => EF (eF_q x) (fmap f # eF_k x)
@@ -135,105 +134,28 @@ Notation App u v := (T (AppF u v)).
 Notation Lam u := (T (LamF u)).
 (* itree term definitions *)
 
-Variant termE : Type -> Type :=
-| VarC : nat -> termE T0
-| AppC : termE T2
-| LamC : termE T1
-.
 
-Equations t_E2F {X} (e : [ termE ]e X) : termF X :=
-  t_E2F (EF (VarC i) k) := VarF i ;
-  t_E2F (EF AppC     k) := AppF (k t2_0) (k t2_1) ;
-  t_E2F (EF LamC     k) := LamF (k t1_0).
+Equations t_rename (f : nat -> nat) (t : term) : term :=
+  t_rename f (Var i) := Var (f i) ;
+  t_rename f (App u v) := App (t_rename f u) (t_rename f v) ;
+  t_rename f (Lam u) := Lam (t_rename f' u)
+    where f' : nat -> nat := {
+          f' O     := O ;
+          f' (S i) := S (f i)}.
 
-Equations t_F2E {X} (e : termF X) : [ termE ]e X :=
-  t_F2E (VarF i) := EF (VarC i) (fun i : T0 => match i with end) ;
-  t_F2E (AppF u v) := EF AppC (fun i : T2 => match i with t2_0 => u
-                                                        | t2_1 => v end) ;
-  t_F2E (LamF u) := EF LamC (fun i : T1 => match i with t1_0 => u end).
-  
-Lemma t_F2E2F {X} (e : termF X) : t_E2F (t_F2E e) = e.
-destruct e; reflexivity.
-Qed.
+Equations t_subst (f : nat -> term) (t : term) : term :=
+  t_subst f (Var i) := f i ;
+  t_subst f (App u v) := App (t_subst f u) (t_subst f v) ;
+  t_subst f (Lam u) := Lam (t_subst f' u)
+    where f' : nat -> term := {
+          f' O     := Var O ;
+          f' (S i) := t_rename S (f i)}.
 
-(* /!\ functional extensionality used here *)
-Lemma t_E2F2E : forall {X} (e : [ termE ]e X), t_F2E (t_E2F e) = e.
-  destruct e as [ R [] k ];
-    rewrite ?t_E2F_equation_1, ?t_F2E_equation_1;
-    rewrite ?t_E2F_equation_2, ?t_F2E_equation_2;
-    rewrite ?t_E2F_equation_3, ?t_F2E_equation_3;
-    apply f_equal, functional_extensionality;
-    destruct x ;
-    reflexivity.
-Qed.
-
-Definition termT {X : Type} := itree termE X.
-Definition termT0 : Type := itree termE T0.
-Definition VarT {X} i : @termT X := wrap (t_F2E (VarF i)).
-Definition AppT {X} u v : @termT X := wrap (t_F2E (AppF u v)).
-Definition LamT {X} u : @termT X := wrap (t_F2E (LamF u)).
-
-Equations t_ind2it : term -> termT0 :=
-  t_ind2it (Var i)   := VarT i ;
-  t_ind2it (App u v) := AppT (t_ind2it u) (t_ind2it v) ;
-  t_ind2it (Lam u)   := LamT (t_ind2it u)
-.
-
-Equations t_it2ind_aux (h : termT0 -> itree void1 term) (t : itree' termE T0) : itree void1 term :=
-  t_it2ind_aux h (RetF x) := boom x ;
-  t_it2ind_aux h (TauF x) := Tau (h x) ;
-  t_it2ind_aux h (VisF (VarC i) k) := ret (Var i) ;
-  t_it2ind_aux h (VisF AppC k) := bind (h (k t2_0)) (fun u =>
-                                  bind (h (k t2_1)) (fun v => ret (App u v)));
-  t_it2ind_aux h (VisF LamC k) := bind (h (k t1_0)) (fun u => ret (Lam u)).
-  
-               
-Definition t_it2ind : termT0 -> itree void1 term :=
-  cofix h t := t_it2ind_aux h (observe t).
-.  cofix h.
-  refine (fun t => t_it2ind_aux h (observe t)).
-  cofix h.
-  refine (fun t => match observe t with
-          | RetF x => boom x
-          | TauF t => h t
-          | VisF e k => ret (T (tmap _ (t_E2F _)))
-          end).
-  refine (iter _).
-  intro t.
-  refine (caseE boom t _).
-  intro x.
-  refine (let y := t_E2F x in _).
-  refine (t_E2F x).
-
-
-Equations t_shift_aux : [ termE ]e (stateT (nat -> nat) (itree termE) T0)
-                      -> stateT (nat -> nat) (itree termE) T0 :=
-  t_shift_aux (EF (VarC i) k) e := VarT (e i) ;
-  t_shift_aux (EF AppC     k) e := AppT (k t2_0 e) (k t2_1 e) ;
-  t_shift_aux (EF LamC     k) e := LamT (k t1_0 (fun i => match i with
-                                                        | O => O
-                                                        | S i => S (e i)
-                                                        end)).
-Definition t_shift : termT0 -> (nat -> nat) -> termT0 :=
-  foldE_ad (fmap inr # t_shift_aux).
-
-
-Equations t_subst_aux : [ termE ]e (stateT (nat -> termT0) (itree termE) T0)
-                      -> stateT (nat -> termT0) (itree termE) T0 :=
-  t_subst_aux (EF (VarC i) k) e := fmap (fun x => pair e x) (e i) ;
-  t_subst_aux (EF AppC k)     e := AppT (k t2_0 e) (k t2_1 e) ;
-  t_subst_aux (EF LamC k)     e := LamT (k t1_0 (fun i => match i with
-                                                  | O => VarT O
-                                                  | S i => t_shift (e i) S
-                                                  end)).
-Definition t_subst : termT0 -> (nat -> termT0) -> termT0 :=
-  foldE_ad (fmap inr # t_subst_aux).
-
-Equations env1 (u : termT0) : nat -> termT0 :=
+Equations env1 (u : term) : nat -> term :=
   env1 u O     := u ;
-  env1 u (S i) := VarT i.
+  env1 u (S i) := Var i.
 
-Definition t_subst1 u v := t_subst u (env1 v).
+Definition t_subst1 u v := t_subst (env1 v) u.
 
 
 (******************************)
@@ -241,52 +163,68 @@ Definition t_subst1 u v := t_subst u (env1 v).
 
 Variant normF (X : Type) : Type :=
 | NLamF : X -> normF X
-| NRedF : nat -> list X -> normF X
+| NAppF : nat -> list X -> normF X
 .
-Arguments NLamF {X}.
-Arguments NRedF {X}.
 
+Arguments NLamF {X}.
+Arguments NAppF {X}.
+
+Variant whn : Type := W : normF term -> whn.
 Inductive norm : Type := N : normF norm -> norm.
 
-Notation NLam u := (N (NLamF u)).
-Notation NRed i xs := (N (NRedF i xs)).
+Search "whn".
 
-Variant normE : Type -> Type :=
-| NLamC : normE T1
-| NRedC : forall i j : nat, normE (fin j)
+Notation WLam u := (W (NLamF u)).
+Notation WApp i xs := (W (NAppF i xs)).
+Notation NLam u := (N (NLamF u)).
+Notation NApp i xs := (N (NAppF i xs)).
+
+Variant boehmE : Type -> Type :=
+| BLamC : boehmE T1
+| BAppC : forall i j : nat, boehmE (fin j)
 .
 
+Equations l_get {X} (xs : list X) : fin (length xs) -> X :=
+  l_get (cons x xs) F0     := x ;
+  l_get (cons x xs) (FS i) := l_get xs i.
+
+Definition BLamT {X} u : itree boehmE X :=
+  Vis BLamC (fun x => match x with t1_0 => u end).
+Definition BAppT {X} i xs : itree boehmE X :=
+  Vis (BAppC i (length xs)) (l_get xs).
+Check (@Vis _ _).
 
 
+Definition normT := itree boehmE T0.
 
-Definition normT {X : Type} := itree normE X.
-Definition normT0 := itree normE T0.
+Equations app_many : term -> list term -> term :=
+  app_many a nil := a ;
+  app_many a (cons x xs) := App (app_many a xs) x.
 
-Definition NLamT {X} u : @normT X :=
-  Vis NLamC (fun x : T1 => match x with t1_0 => u end).
-Definition NRedT {X} (i j : nat) (f : fin j -> @normT X) : @normT X :=
-  Vis (NRedC i j) f.
+Equations term_of_whn (w : whn) : term :=
+  term_of_whn (WLam u) := Lam u ;
+  term_of_whn (WApp i xs) := app_many (Var i) xs.
 
-Equations app_many (n : nat) : termT0 -> (fin n -> termT0) -> termT0 :=
-  app_many O     x f := x ;
-  app_many (S n) x f := AppT (app_many n x (f # FS)) (f F0).
+Definition eval_whn : term -> itree void1 whn :=
+  rec (fun t =>
+        match t with
+        | Var i => ret (WApp i nil)
+        | App u v =>
+          u' <- call u ;;
+          v' <- call v ;;
+          match u' with
+          | WLam w => call (t_subst1 w (term_of_whn v'))
+          | WApp i xs => ret (WApp i (cons (term_of_whn v') xs))
+          end
+        | Lam u => ret (WLam u)
+        end).
 
-Equations norm2term_aux : [ normE ]e termT0 -> termT0 :=
-  norm2term_aux (EF NLamC k)       := LamT (k t1_0);
-  norm2term_aux (EF (NRedC i j) k) := app_many j (VarT i) k.
+(* w_inj k (W x) := wrap (F2E (fmap k x)) *)
+(* this is because normF == [ boehmE ]e *)
+Equations w_inj {X} : (term -> itree boehmE X) -> whn -> itree boehmE X :=
+  w_inj k (WLam u) := Vis BLamC (fun x => match x with t1_0 => k u end);
+  w_inj k (WApp i xs) := Vis (BAppC i (length xs)) (k # l_get xs).
 
-Definition norm2term : normT0 -> termT0 :=
-  foldE (fmap inr # norm2term_aux).
-
-Equations eval_aux : [ termE ]e normT0 -> itree normE (termT0 + T0) :=
-  eval_aux (EF (VarC i) k) := NRedT i 0 (fun i => match i with end) ;
-  eval_aux (EF LamC k) := NLamT (fmap inr (k t1_0)) ;
-  eval_aux (EF AppC k) := caseE inr (k t2_0) app_case
-  where app_case : [ normE ]e _ ->  _ :=  {
-        app_case (EF NLamC       k1) := ret (inl (t_subst1 (norm2term (k1 t1_0))
-                                                           (norm2term (k t2_1)))) ;
-        app_case (EF (NRedC i j) k1) := vis (NRedC i (S j)) red_arg
-        where red_arg : fin (S j) -> _ := {
-              red_arg F0     := fmap inr (k t2_1) ;
-              red_arg (FS i) := fmap inr (k1 i) }}.
-Definition eval : termT0 -> normT0 := foldE eval_aux.
+(* magic :D *)
+Definition eval_boehmE : term -> normT :=
+  iter (fun t => bind (translate elim_void1 (eval_whn t)) (w_inj (ret # inl))).
