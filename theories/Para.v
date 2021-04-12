17948c3fbc558c6dc5101a7dd2e9383cb955845a
Yannick <y.zakowski@gmail.com>
2021-04-12 17:06:17 +0200

    Alternate synchronization strategy and elements of interpretation of schedulers

--- theories/Para.v
+++ theories/Para.v
@@ -1,8 +1,9 @@
 From ITree Require Import
-     Events.Dependent
      ITree
-     Events.Exception.
-
+     Eq.Eq
+     Events.Exception
+     ITreeFacts.
+Require Import OGS.PropT.
 Import ITreeNotations.
 
 (** * get_hd
@@ -49,6 +50,18 @@ Section Interleave.
                          end)
   . 
 
+  (** * Implementing the non-determinism induced by the underlying scheduler *)
+
+  (* Collecting propositionally all computations *)
+
+  Variant schedulers {E} : SchedE ~> PropT E :=
+  | SchedL : schedulers _ Sched (Ret true)
+  | SchedR : schedulers _ Sched (Ret false).
+
+  Definition schedule {E} : itree (SchedE +' E) ~> PropT E :=
+    fun T => interp_prop (case_ schedulers trigger_prop) T eq.
+
+  (* Could alternatively parameterize the handler by a scheduler given by a stream of bits and quantify at the meta level over all schedules *)
 
 End Interleave.
 
@@ -76,13 +89,31 @@ Section Synchro.
   Definition para_synch : itree E1 X1 -> itree E2 X2 -> itree ((E1 *' E2) +' FailureE) (X1 * X2) :=
     cofix para_synch (t1 : itree E1 X1) (t2 : itree E2 X2) :=
       match observe t1, observe t2 with
-      | TauF t1, TauF t2 => Tau (para_synch t1 t2) 
-      | TauF t1, _ => Tau (para_synch t1 t2) 
-      | _, TauF t2 => Tau (para_synch t1 t2)
-      | VisF e1 k1, VisF e2 k2 => vis (pair1 e1 e2)
-                                     (fun '(x1,x2) => para_synch (k1 x1) (k2 x2))
-      | RetF x1, RetF x2 => Ret (x1,x2)
-      | _, _ => throw
+      | TauF t1, TauF t2       => Tau (para_synch t1 t2) 
+      | TauF t1, _             => Tau (para_synch t1 t2) 
+      | _, TauF t2             => Tau (para_synch t1 t2)
+      | VisF e1 k1, VisF e2 k2 => vis (pair1 e1 e2) (fun '(x1,x2) => para_synch (k1 x1) (k2 x2))
+      | RetF x1, RetF x2       => Ret (x1,x2)
+      | _, _                   => throw
       end.
+
+  (* Alternate parallel "strong" synchronization of two computations [t1] and [t2] ("zipper"):
+     - Events are still synchronous, each computation waits for the other at such point
+     - But the first terminating computation is interpreted as a successful computation.
+     - The computation is still deterministic, and cannot fail anymore
+   *)
+
+  Definition para_synch' : itree E1 X1 -> itree E2 X2 -> itree (E1 *' E2) (X1 + X2 + (X1 * X2)) :=
+    cofix para_synch (t1 : itree E1 X1) (t2 : itree E2 X2) :=
+      match observe t1, observe t2 with
+      | RetF x1, RetF x2       => Ret (inr (x1,x2))
+      | RetF x1, _             => Ret (inl (inl x1))
+      | _, RetF x2             => Ret (inl (inr x2))
+      | TauF t1, TauF t2       => Tau (para_synch t1 t2) 
+      | TauF t1, _             => Tau (para_synch t1 t2) 
+      | _, TauF t2             => Tau (para_synch t1 t2)
+      | VisF e1 k1, VisF e2 k2 => vis (pair1 e1 e2) (fun '(x1,x2) => para_synch (k1 x1) (k2 x2))
+      end.
+
 
 End Synchro.
