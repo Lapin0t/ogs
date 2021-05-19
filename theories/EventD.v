ebcce2d021382a3518ba6045a87fb51c565d43ee
Lapinot <peio.borthelle@ens-lyon.fr>
2021-05-19 19:37:14 +0200

    moar comments

--- theories/EventD.v
+++ theories/EventD.v
@@ -20,6 +20,8 @@ Instance FunctorEvent {I} (E : event I) : Functor (⟦ E ⟧).
   exact (existT _ e (fun r => f _ (k r))).
 Defined.
 
+(****************)
+(* event arrows *)
 Definition e_arrow {I : Type} (E : event I) (F : endo (psh I)) : Type :=
   forall i (q : E.(qry) i), F (fiber (E.(nxt) q)) i.
 
@@ -39,13 +41,35 @@ Definition e_arrow_mk {I} {E : event I} {F : endo (psh I)} {FF : Functor F}
            : ⟦ E ⟧ ⟹ F -> E ₑ⇒ F :=
   fun m i q => m (fiber (nxt E q)) _ (existT _ q Fib).
 
-(* alternate *)
+(*********************************************)
+(* alternate unrolled definition of 'E ⇒ₑ F' *)
 Record e_arrow' {I : Type} (E F : event I) := EArrow {
   EA_qry {i} : E.(qry) i -> F.(qry) i ;
   EA_rsp {i} (q : E.(qry) i) : F.(rsp) (EA_qry q) -> E.(rsp) q ;
   EA_coh {i} (q : E.(qry) i) r : E.(nxt) q (EA_rsp q r) = F.(nxt) (EA_qry q) r
 }.
 
+Definition e_arrow'_to_arrow {I} {E F : event I} : e_arrow' E F -> E ⇒ₑ F.
+intros m i q.
+refine (existT _ (m.(EA_qry) q) (fun r => _)).
+rewrite <-(m.(EA_coh) q r).
+exact (Fib (m.(EA_rsp) q r)).
+Defined.
+
+Definition e_arrow_to_arrow' {I} {E F : event I} : E ⇒ₑ F -> e_arrow' E F.
+intros m.
+refine (EArrow (fun _ q => projT1 (m _ q))
+               (fun _ q r => let (a) := projT2 (m _ q) r in a)
+                _).
+intros; cbn; destruct (projT2 (m i q) r); reflexivity.
+Defined.
+Print e_arrow_to_arrow'.
+
+
+(***************************)
+(* constructions on events *)
+
+(* event coproduct *)
 Definition esum {I : Type} (E F : event I) : event I.
   unshelve refine (Event _ _ _).
   + intro i. exact (E.(qry) i + F.(qry) i)%type.
@@ -57,8 +81,17 @@ Definition esum {I : Type} (E F : event I) : event I.
     - exact (F.(nxt) q r).
 Defined.
 
+(* null event*)
+Definition evoid {I : Type} : event I :=
+  Event (fun _ => T0) (fun _ e => match e with end) (fun _ e => match e with end).
+
+(* unit event *)
 Definition eunit {I : Type} : event I :=
   Event (fun _ => T1) (fun _ _ => T1) (fun i _ _ => i).
 
-Definition evoid {I : Type} : event I :=
-  Event (fun _ => T0) (fun _ e => match e with end) (fun _ e => match e with end).
+(* translate a event from deepspec-itree form to trivially indexed form *)
+Definition e_from_original (E : Type -> Type) : event T1 :=
+  Event (fun _ => { X : Type & E X })
+        (fun _ x => projT1 x)
+        (fun _ _ _ => t1_0).
+
