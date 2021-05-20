31201fee58d41356c9a518b9899d992b3ad5ea3f
Lapinot <peio.borthelle@ens-lyon.fr>
2021-05-20 16:46:08 +0200

    port recursive combinators

--- theories/RecD.v
+++ theories/RecD.v
@@ -1,8 +1,8 @@
 From OGS Require Import Utils EventD ITreeD CatD.
 Check esum.
 
-Definition iter {I} {E : event I} {X Y : I -> Type} (body : X ==> itree E (X +i Y))
-             : X ==> itree E Y :=
+Definition iter {I} {E : event I} {X Y : I -> Type} (body : X ⇒ᵢ itree E (X +ᵢ Y))
+  : X ⇒ᵢ itree E Y :=
   cofix _iter i x :=
     body i x ?>= fun _ y => match y with
                             | inl x' => Tau (_iter _ x')
@@ -11,43 +11,33 @@ Definition iter {I} {E : event I} {X Y : I -> Type} (body : X ==> itree E (X +i 
 
 Definition interp {I} {E : event I}
            {M : psh I -> psh I} {MF : Functor M} {MM : Monad M} {MI : MonadIter M}
-           (h : earrow_fam E M) X
-  : itree E X ==> M X.
-  refine (CatD.iter _).
-  refine (fun i x => _).
-  refine (match (observe x) with
-          | RetF x => CatD.ret _ (inr x)
-          | TauF t => CatD.ret _ (inl t)
-          | VisF e k => CatD.fmap (fun _ => inl) _ _
-          end).
-  refine (ea_eval_fam h _ _ (existT _ e k)).
-Defined.
+           (h : E ₑ⇒ M) X
+  : itree E X ⇒ᵢ M X :=
+  CatD.iter (fun i x => match (observe x) with
+            | RetF x => CatD.ret _ (inr x)
+            | TauF t => CatD.ret _ (inl t)
+            | VisF e k => CatD.fmap (fun _ => inl) _ (e_arrow_eval h _ _ (existT _ e k))
+            end).
 
 Definition interp_mrec {I} {E F : event I}
-           (body : earrow_fam E (itree (esum E F)))
-           {X} : itree (esum E F) X ==> itree F X.
-  refine (iter (fun _ t => match (observe t) with
-                | RetF r => Ret (inr r)
-                | TauF t => Ret (inl t)
-                | VisF q k => _
-                end)).
-  destruct q.
-  - exact (Ret (inl (body _ q ?>= (fun _ o => match o with FOk _ r => k r end)))).
-  - exact (Vis q (fun r => Ret (inl (k r)))).
-Defined.
+           (body : E ₑ⇒ itree (esum E F))
+           : itree (esum E F) ⟹ itree F :=
+  fun _ => iter (fun _ (t : itree (esum E F) _ _) => match (observe t) with
+              | RetF r => Ret (inr r)
+              | TauF t => Ret (inl t)
+              | VisF (inl q) k => Ret (inl (body _ q ?>= fiber_into _ k))
+              | VisF (inr q) k => Vis q (fun r => Ret (inl (k r)))
+              end).
 
-Definition interp_mrec' {I} {E F : event I}
-           (body : forall X, ⟦ E ]] X ==> itree (esum E F) X)
-           {X} : itree (esum E F) X ==> itree F X.
-  refine (iter (fun _ t => match (observe t) with
-                | RetF r => Ret (inr r)
-                | TauF t => Ret (inl t)
-                | VisF q k => _
-                end)).
-  destruct q.
-  - exact (Ret (inl (body _ q ?>= (fun _ o => match o with FOk _ r => k r end)))).
-  - exact (Vis q (fun r => Ret (inl (k r)))).
-Defined.
-Definition mrec {I} {E F : event I} (body : earrow_fam E (itree (esum E F)))
-           : earrow_fam E (itree F) :=
-  fun _ q => interp_mrec body _ (body _ q).
+Definition mrec {I} {E F : event I} (body : E ₑ⇒ itree (esum E F)) : E ₑ⇒ itree F :=
+  fun _ q => interp_mrec body _ _ (body _ q).
+
+Definition ecall (A : Type) (B : A -> Type) : event T1 :=
+  Event (fun _ => A) (fun _ => B) (fun i _ _ => i).
+
+Definition rec {A B} (body : forall a, itree (ecall A B) (fun _ => B a) t1_0) : forall a, itree evoid (fun _ => B a) t1_0.
+  refine (fun a => _).
+  refine (@mrec T1 (ecall A B) evoid _ t1_0 _ ?>= _).
+  intros i q.
+  cbn in q.
+  refine (((fun _ => inl) <$> body a)).
