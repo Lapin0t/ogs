a679c96dac5fc4894ec837bcf8b71689ca5ee657
Lapinot <peio.borthelle@ens-lyon.fr>
2021-06-07 13:31:51 +0200

    Generalize events to non-endo functors.

--- theories/EventD.v
+++ theories/EventD.v
@@ -4,121 +4,139 @@ Set Primitive Projections.
 
 From OGS Require Import Utils CatD.
 
-Record event (I : Type) : Type := Event {
-  qry : I -> Type ;
-  rsp {i} : qry i -> Type ;
-  nxt {i} (q : qry i) : rsp q -> I
+Record event (I J : Type) : Type := Event {
+  qry : J -> Type ;
+  rsp {j} : qry j -> Type ;
+  nxt {j} (q : qry j) : rsp q -> I
 }.
 
-Definition e_eval {I} (E : event I) : endo (psh I) :=
-  fun X i => { q : E.(qry) i & forall r : E.(rsp) q, X (E.(nxt) q r) }.
+Definition e_eval {I J} (E : event I J) : psh I -> psh J :=
+  fun X i => { q : qry E i & forall r : rsp E q, X (nxt E q r) }.
 
 Notation "⟦ E ⟧" := (e_eval E) (at level 20).
+Definition visₑ {I J} {E : event I J} {X i} q k : ⟦ E ⟧ X i := existT _ q k.
+Arguments visₑ : clear implicits.
+Arguments visₑ {I J E X i} q k.
 
-Instance FunctorEvent {I} (E : event I) : Functor (⟦ E ⟧).
+Instance FunctorEvent {I J} (E : event I J) : Functor (⟦ E ⟧).
   econstructor. intros x y f i [e k].
-  exact (existT _ e (fun r => f _ (k r))).
+  exact (visₑ e (fun r => f _ (k r))).
 Defined.
 
 (****************)
 (* event arrows *)
-Definition e_arrow {I : Type} (E : event I) (F : endo (psh I)) : Type :=
-  forall i (q : E.(qry) i), F (fiber (E.(nxt) q)) i.
+Definition e_arrow {I J : Type} (E : event I J) (F : psh I -> psh J) : Type :=
+  forall j (q : E.(qry) j), F (fiber (E.(nxt) q)) j.
 
 Notation "E ₑ⇒ F" := (e_arrow E F) (at level 30).
 Notation "E ⇒ₑ F" := (E ₑ⇒ ⟦ F ⟧) (at level 30).
 
-Definition e_arrow_eval {I} {E : event I} {F : endo (psh I)} {FF : Functor F}
+Definition e_arrow_eval {I J} {E : event I J} {F : psh I -> psh J} {FF : Functor F}
            : E ₑ⇒ F -> ⟦ E ⟧ ⟹ F :=
   fun m _ _ x => fiber_into _ (projT2 x) <$> m _ (projT1 x).
-Arguments e_arrow_eval {I} {E} {F} {FF} m X.
+Arguments e_arrow_eval {I J E F FF} m X.
 Notation "⟦⇒ m ⟧ X" := (e_arrow_eval m X) (at level 30).
 
-(* inverse of e_arrow_eval, meaning that the functor ⟦-⟧ : event I → endo (psh I)
-   is fully-faithfull (i think the proof requires function extensionality
-   and parametricity) *)
-Definition e_arrow_mk {I} {E : event I} {F : endo (psh I)} {FF : Functor F}
-           : ⟦ E ⟧ ⟹ F -> E ₑ⇒ F :=
+(** inverse of e_arrow_eval, meaning that the functor ⟦-⟧ : event I → endo (psh I)
+    is fully-faithfull (i think the proof requires function extensionality
+    and parametricity) *)
+Definition e_arrow_mk {I J} {E : event I J} {F} : ⟦ E ⟧ ⟹ F -> E ₑ⇒ F :=
   fun m i q => m (fiber (nxt E q)) _ (existT _ q Fib).
 
-(*********************************************)
-(* alternate unrolled definition of 'E ⇒ₑ F' *)
-Record e_arrow' {I : Type} (E F : event I) := EArrow {
-  EA_qry {i} : E.(qry) i -> F.(qry) i ;
-  EA_rsp {i} (q : E.(qry) i) : F.(rsp) (EA_qry q) -> E.(rsp) q ;
-  EA_coh {i} (q : E.(qry) i) r : E.(nxt) q (EA_rsp q r) = F.(nxt) (EA_qry q) r
+
+(** Alternate unrolled definition of 'E ⇒ₑ F' *)
+Record e_arrow' {I J : Type} (E F : event I J) := EArrow {
+  EA_qry {j} : qry E j -> qry F j ;
+  EA_rsp {j} (q : qry E j) : rsp F (EA_qry q) -> rsp E q ;
+  EA_coh {j} (q : qry E j) r : nxt E q (EA_rsp q r) = nxt F (EA_qry q) r
 }.
+Notation "E ⇒'ₑ F" := (e_arrow' E F) (at level 30).
 
-Definition e_arrow'_to_arrow {I} {E F : event I} : e_arrow' E F -> E ⇒ₑ F.
-intros m i q.
-refine (existT _ (m.(EA_qry) q) (fun r => _)).
-rewrite <-(m.(EA_coh) q r).
-exact (Fib (m.(EA_rsp) q r)).
-Defined.
+Definition e_arrow'_to_arrow {I J} {E F : event I J} (m : E ⇒'ₑ F) : E ⇒ₑ F :=
+  fun j q => visₑ (EA_qry m q) (fun r => fiber_mk _ _ (EA_coh m q r)).
 
-Definition e_arrow_to_arrow' {I} {E F : event I} : E ⇒ₑ F -> e_arrow' E F.
-intros m.
-refine (EArrow (fun _ q => projT1 (m _ q))
-               (fun _ q r => let (a) := projT2 (m _ q) r in a)
-                _).
-intros; cbn; destruct (projT2 (m i q) r); reflexivity.
-Defined.
-Print e_arrow_to_arrow'.
+Definition e_arrow_to_arrow' {I J} {E F : event I J} (m : E ⇒ₑ F) : E ⇒'ₑ F :=
+  EArrow (fun j q => projT1 (m j q))
+         (fun j q r => fiber_ext (projT2 (m j q) r))
+         (fun j q r => fiber_coh (projT2 (m j q) r)).
 
+(************************)
 (* non-dependent events *)
 
-Definition event0 : Type := event T1.
-Definition e_eval0 (E : event0) : Type -> Type := fun X => ⟦ E ⟧ (fun _ => X) t1_0.
-Definition e_arrow0 (E : event0) (F : Type -> Type) : Type :=
-  forall q : E.(qry) t1_0, F (E.(rsp) q).
-Notation "E ₑ⇒₀ F" := (e_arrow0 E F) (at level 30).
-Definition lift_fam0 (F : Type -> Type) (X : psh T1) : psh T1 := fun _ => F (X t1_0).
-(*
-Definition arrow_of_arrow0 {E F} {FF : Functor (lift_fam0 F)} (m : E ₑ⇒₀ F) : E ₑ⇒ (lift_fam0 F).
-  intros [] q.
-  unfold e_arrow0 in m.
-  Check (@fmap T1 (lift_fam0 F) FF).
-*)
+Definition lower₀ (F : psh T1 -> psh T1) (X : Type) : Type := F (fun _ => X) t1_0.
+Definition lift₀ (F : Type -> Type) (X : psh T1) : psh T1 := fun _ => F (X t1_0).
+
+(* type *)
+Definition event₀ : Type := event T1 T1.
+
+(* constructor and projections *)
+Definition Event₀ (S : Type) (A : S -> Type) : event₀ :=
+  Event (fun _ => S) (fun _ s => A s) (fun i _ _ => i).
+Arguments Event₀ : clear implicits.
+Definition qry₀ (E : event₀) : Type := E.(qry) t1_0.
+Definition rsp₀ (E : event₀) : qry₀ E -> Type := E.(rsp).
+Definition nxt₀ (E : event₀) (q : qry₀ E) r : E.(nxt) q r = t1_0 :=
+  match E.(nxt) q r with t1_0 => eq_refl end.
+
+Notation "⟦₀ E ⟧ X" := (lower₀ (⟦ E ⟧) X) (at level 30).
+
+Definition e_arrow₀ (E : event₀) (F : Type -> Type) : Type := forall q, F (rsp₀ E q).
+Notation "E ₑ⇒₀ F" := (e_arrow₀ E F) (at level 30).
   
 
 (***************************)
 (* constructions on events *)
 
-(* event coproduct *)
-Definition esum {I : Type} (E F : event I) : event I.
-  unshelve refine (Event _ _ _).
-  + intro i. exact (E.(qry) i + F.(qry) i)%type.
-  + intros i q. destruct q.
-    - exact (E.(rsp) q).
-    - exact (F.(rsp) q).
-  + intros i q r. destruct q.
-    - exact (E.(nxt) q r).
-    - exact (F.(nxt) q r).
-Defined.
+(** event is a bifunctor 'Type ⊗ Typeᵒᵖ → Type' with the following two actions *)
+Definition e_lift_r {I J K} (f : K -> J) (E : event I J) : event I K :=
+  Event (E.(qry) ∘ f) (fun _ => E.(rsp)) (fun _ => E.(nxt)).
+Definition e_lift_l {I J K} (f : I -> K) (E : event I J) : event K J :=
+  Event E.(qry) (fun _ => E.(rsp)) (fun _ q => f ∘ E.(nxt) q).
 
-Definition einl {I} {E F : event I} : E ⇒ₑ esum E F :=
-  fun i q => existT _ (inl q) Fib.
-Definition einr {I} {E F : event I} : F ⇒ₑ esum E F :=
-  fun i q => existT _ (inr q) Fib.
+Notation "E <ₑ f" := (e_lift_r f E) (at level 30).
+Notation "f >ₑ E" := (e_lift_l f E) (at level 30).
+
+
+(** event coproduct *)
+Definition esum {I J : Type} (E F : event I J) : event I J :=
+  Event (E.(qry) +ᵢ F.(qry))
+        (fun _ q => match q with
+                 | inl q => E.(rsp) q
+                 | inr q => F.(rsp) q
+                 end)
+        (fun _ q => match q as q
+                 return match q with
+                         | inl q => E.(rsp) q
+                         | inr q => F.(rsp) q
+                         end -> I
+                 with
+                 | inl q => E.(nxt) q
+                 | inr q => F.(nxt) q
+                 end).
+Notation "E +ₑ F" := (esum E F) (at level 30).
+
+Definition inlₑ {I J} {E F : event I J} : E ⇒ₑ (E +ₑ F) :=
+  fun _ q => existT _ (inl q) Fib.
+Definition inrₑ {I J} {E F : event I J} : F ⇒ₑ (E +ₑ F) :=
+  fun _ q => existT _ (inr q) Fib.
 
 
 (* null event*)
-Definition evoid {I : Type} : event I :=
-  Event (fun _ => T0) (fun _ e => match e with end) (fun _ e => match e with end).
-Definition eelim0 {I} {E : event I} : evoid ⇒ₑ E :=
-  fun _ q => ex_falso q.
+Definition evoid {I J : Type} : event I J :=
+  Event (fun _ => T0) (fun _ q => match q with end) (fun _ q => match q with end).
+Notation "∅ₑ" := (evoid).
+Definition eelim0 {I J} {E : event I J} : evoid ⇒ₑ E := fun _ q => match q with end.
 
 (* unit event *)
-Definition eunit {I : Type} : event I :=
+Definition eunit {I : Type} : event I I :=
   Event (fun _ => T1) (fun _ _ => T1) (fun i _ _ => i).
+Notation "1ₑ" := (eunit).
 
 (* translate a event from deepspec-itree form to trivially indexed form *)
-Definition eclassic (E : Type -> Type) : event T1 :=
-  Event (fun _ => { X : Type & E X })
-        (fun _ x => projT1 x)
-        (fun _ _ _ => t1_0).
+Definition eclassic (E : Type -> Type) : event₀ :=
+  Event₀ { X : Type & E X } (@projT1 _ _).
 
-Definition e_of_classic {E : Type -> Type} {X} : E X -> ⟦ eclassic E ⟧ (fun _ => X) t1_0.
+Definition e_of_classic {E : Type -> Type} {X} : E X -> ⟦₀ eclassic E ⟧ X.
   refine (fun e => existT _ (existT E X e) _).
   refine (fun x => x).
 Defined.
--- theories/ITreeD.v
+++ theories/ITreeD.v
@@ -8,7 +8,7 @@ From OGS Require Import CatD Utils EventD.
 
 Section itree.
 
-  Context {I : Type} {E : event I} {R : I -> Type}.
+  Context {I : Type} {E : event I I} {R : I -> Type}.
 
   Variant itreeF (ind : I -> Type) (i : I) :=
   | RetF (r : R i)
@@ -38,14 +38,14 @@ Notation Tau t := (go (TauF t)).
 Notation Vis e k := (go (VisF e k)).
 
 Section a.
-Context {I} {E : event I} {R : I -> Type}.
+Context {I} {E : event I I} {R : I -> Type}.
 Definition ret {i} x : itree E R i := Ret x.
 Definition tau {i} t : itree E R i := Tau t.
 Definition vis {i} e k : itree E R i := Vis e k.
-Definition vis' : ⟦ E ⟧ (itree E R) ⇒ᵢ itree E R := fun i x => Vis _ (projT2 x).
+Definition vis' : ⟦ E ⟧ (itree E R) ⇒ᵢ itree E R := fun _ x => Vis _ (projT2 x).
 End a.
 
-Definition fmap {I} {E : event I} {X Y} (f : X ⇒ᵢ Y) : itree E X ⇒ᵢ itree E Y :=
+Definition fmap {I} {E : event I I} {X Y} (f : X ⇒ᵢ Y) : itree E X ⇒ᵢ itree E Y :=
   cofix _fmap _ u :=
     match (observe u) with
     | RetF r => Ret (f _ r)
@@ -53,7 +53,7 @@ Definition fmap {I} {E : event I} {X Y} (f : X ⇒ᵢ Y) : itree E X ⇒ᵢ itre
     | VisF e k => Vis e (fun r => _fmap _ (k r))
     end.
 
-Definition subst {I} {E : event I} {X Y} (f : X ⇒ᵢ itree E Y)
+Definition subst {I} {E : event I I} {X Y} (f : X ⇒ᵢ itree E Y)
                  : itree E X ⇒ᵢ itree E Y :=
   cofix _subst _ u :=
     match observe u with
@@ -64,10 +64,10 @@ Definition subst {I} {E : event I} {X Y} (f : X ⇒ᵢ itree E Y)
 
 (* McBride's "daemonic" bind: [f] has to be defined at every state [i] *)
 Definition bind {I E X Y i} x f := @subst I E X Y f i x.
-Notation "x ?>= f" := (bind x f) (at level 20).
+(*Notation "x ?>= f" := (bind x f) (at level 20).*)
 
-Instance FunctorItree {I} (E : event I) : Functor (itree E) :=
+Instance FunctorItree {I} (E : event I I) : Functor (itree E) :=
   Build_Functor _ (fun X Y => @fmap I E X Y).
 
-Instance MonadItree {I} (E : event I) : Monad (itree E) :=
+Instance MonadItree {I} (E : event I I) : Monad (itree E) :=
   Build_Monad _ (fun X => @ret I E X) (fun X Y => @subst I E X Y).
