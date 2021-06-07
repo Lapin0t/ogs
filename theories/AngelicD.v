64d2dc140526852176469bd740cc7207aac32320
Lapinot <peio.borthelle@ens-lyon.fr>
2021-06-07 13:34:08 +0200

    angelic + non-indexed helpers rework

--- theories/AngelicD.v
+++ theories/AngelicD.v
@@ -1,26 +1,36 @@
-From OGS Require Import Utils EventD.
-From OGS Require ITreeD.
+From OGS Require Import Utils EventD CatD.
+From OGS Require Import ITreeD.
+
+Definition itreeₐ {I} (E : event I I) i j X : Type := itree E (X @ j) i.
 
 Section angelic.
-Context {I : Type}.
+Context {I : Type} {E : event I I}.
 
-Definition itree (E : event I) i j X : Type := ITreeD.itree E (X @ j) i.
+Definition tauₐ {X i j} (t : itreeₐ E i j X) : itreeₐ E i j X := tau t.
+Definition retₐ {X i} (x : X) : itreeₐ E i i X := ret (pin _ x).
+Definition visₐ {X i j} q (k : forall r, itreeₐ E _ j X) : itreeₐ E i j X := vis q k.
 
-Definition tau {E : event I} {X i j} (t : itree E i j X) : itree E i j X :=
-  ITreeD.tau t.
-Definition ret {E : event I} {X i} (x : X) : itree E i i X := ITreeD.ret (pin _ x).
-Definition vis {E : event I} {X i j} q (k : forall r, itree E _ j X) : itree E i j X :=
-  ITreeD.vis q k.
+Definition bindₐ {X Y i j k} (x : itreeₐ E i j X) (f : X -> itreeₐ E j k Y)
+                       : itreeₐ E i k Y :=
+ x >>= fiber_into (itree _ _) f.
 
-(* McBride's "angelic" bind: we know the states i, j, k we are in *)
-Definition bind {E : event I} {X Y i j k}
-            : itree E i j X -> (X -> itree E j k Y) -> itree E i k Y :=
- fun x f => ITreeD.bind x (fiber_into (ITreeD.itree E (Y @ _)) f).
+Notation "x !>= f" := (bindₐ x f) (at level 30).
 
-Definition iter {E : event I} {X Y : Type} {i}
-           (body : X -> itree E i i (X + Y)) : X -> itree E i i Y :=
-  cofix _iter x := bind (body x) (fun r => match r with
-                                 | inl x => tau (_iter x)
-                                 | inr y => ret y
-                                 end).
+Definition iterₐ {X Y i} (f : X -> itreeₐ E i i (X + Y)) : X -> itreeₐ E i i Y :=
+  cofix _iter x := f x !>= fun r => match r with
+                                 | inl x => tauₐ (_iter x)
+                                 | inr y => retₐ y
+                                 end.
 End angelic.
+
+
+Definition itree₀ (E : event₀) X : Type := itreeₐ E t1_0 t1_0 X.
+
+Section triv_index.
+  Context {E : event₀}.
+
+  Definition tau₀ {X} (t : itree₀ E X) : itree₀ E X := tau t.
+  Definition ret₀ {X} (x : X) : itree₀ E X := ret (pin _ x).
+  Definition vis₀ {X} (q : qry₀ E) (k : forall r, itree₀ E X) : itree₀ E X :=
+    vis q (fun r => match E.(nxt) q r with t1_0 => k r end).
+End triv_index.
--- theories/RecD.v
+++ theories/RecD.v
@@ -1,19 +1,19 @@
-From OGS Require Import Utils EventD ITreeD CatD.
+From OGS Require Import Utils EventD ITreeD CatD AngelicD.
 Check esum.
 
-Definition iter {I} {E : event I} {X Y : I -> Type} (body : X ⇒ᵢ itree E (X +ᵢ Y))
+Definition iter {I} {E : event I I} {X Y : I -> Type} (body : X ⇒ᵢ itree E (X +ᵢ Y))
   : X ⇒ᵢ itree E Y :=
   cofix _iter i x :=
-    body i x ?>= fun _ y => match y with
+    bind (body i x) (fun _ y => match y with
                             | inl x' => Tau (_iter _ x')
                             | inr y => Ret y
-                            end.
+                            end).
 
-Instance ITreeMonadIter {I} (E : event I) : MonadIter (itree E) :=
+Instance ITreeMonadIter {I} (E : event I I) : MonadIter (itree E) :=
   Build_MonadIter _ (fun X Y => @iter I E X Y).
 
-Definition translate {I} {E F : event I} (f : E ⇒ₑ F) : itree E ⟹ itree F :=
-  fun X => cofix _translate _ u :=
+Definition translate {I} {E F : event I I} (f : E ⇒ₑ F) : itree E ⟹ itree F :=
+  fun _ => cofix _translate _ u :=
     match (observe u) with
     | RetF x => Ret x
     | TauF t => Tau (_translate _ t)
@@ -21,43 +21,65 @@ Definition translate {I} {E F : event I} (f : E ⇒ₑ F) : itree E ⟹ itree F 
                  Vis e1 (fun r => match k1 r with Fib a => _translate _ (k a) end)
      end.
 
-Definition interp {I} {E : event I}
+Definition translate_fwd {I J} {E : event I I} {F : event J J} (f0 : I -> J)
+           (f : (f0 >ₑ E) ⇒ₑ (F <ₑ f0))
+           X : (itree E (X ∘ f0)) ⇒ᵢ (itree F X ∘ f0) :=
+  cofix _translate i u :=
+    match (observe u) with
+    | RetF x => @ITreeD.ret _ _ _ (f0 i) x
+    | TauF t => @ITreeD.tau _ _ _ (f0 i) (_translate _ t)
+    | VisF e k => let (e1, k1) := f _ e in
+                 Vis e1 (fun r => fiber_rect _ _ _ (fun i _ => itree _ _ i)
+                                 (fun a => _translate _ (k a)) _ (k1 r))
+    end.
+
+Definition translate_bwd {I J} {E : event I I} {F : event J J} (f0 : J -> I)
+           (f : (E <ₑ f0) ⇒ₑ (f0 >ₑ F))
+           X : (itree E X ∘ f0) ⇒ᵢ (itree F (X ∘ f0)) :=
+  cofix _translate i u :=
+    match (observe u) with
+    | RetF x => Ret x
+    | TauF t => Tau (_translate _ t)
+    | VisF e k => let (e1, k1) := f _ e in
+                 Vis e1 (fun r => _translate _ (fiber_rect _ _ _ (fun i _ => itree _ _ i)
+                                                        k _ (k1 r)))
+    end.
+
+Definition emb_comp {I} {E : event I I} X i : itree₀ ∅ₑ X -> itree E (X @ i) i.
+  refine (fun t => translate_fwd (fun _ => i) (fun _ (i : qry (_ >ₑ ∅ₑ) _) => ex_falso i) (X @ i) t1_0 (t >>= _)).
+  refine (fun 't1_0 '(Fib a) => Ret (Fib a)).
+Defined.
+
+Definition interp {I} {E : event I I}
            {M : psh I -> psh I} {MF : Functor M} {MM : Monad M} {MI : MonadIter M}
-           (h : E ₑ⇒ M) X
-  : itree E X ⇒ᵢ M X :=
-  CatD.iter (fun i x => match (observe x) with
+           (h : E ₑ⇒ M)
+           : itree E ⟹ M :=
+  fun _ => CatD.iter (fun i x => match (observe x) with
             | RetF x => CatD.ret _ (inr x)
             | TauF t => CatD.ret _ (inl t)
             | VisF e k => CatD.fmap (fun _ => inl) _ (e_arrow_eval h _ _ (existT _ e k))
             end).
 
 
-Definition interp_mrec {I} {E F : event I}
+Definition interp_mrec {I} {E F : event I I}
            (body : E ₑ⇒ itree (esum E F))
            : itree (esum E F) ⟹ itree F :=
   fun _ => iter (fun _ (t : itree (esum E F) _ _) => match (observe t) with
               | RetF r => Ret (inr r)
               | TauF t => Ret (inl t)
-              | VisF (inl q) k => Ret (inl (body _ q ?>= fiber_into _ k))
+              | VisF (inl q) k => Ret (inl (body _ q >>= fiber_into _ k))
               | VisF (inr q) k => Vis q (fun r => Ret (inl (k r)))
               end).
 
-Definition mrec {I} {E F : event I} (body : E ₑ⇒ itree (esum E F)) : E ₑ⇒ itree F :=
+Definition mrec {I} {E F : event I I} (body : E ₑ⇒ itree (esum E F)) : E ₑ⇒ itree F :=
   fun _ q => interp_mrec body _ _ (body _ q).
 
-Definition ecall (A : Type) (B : A -> Type) : event T1 :=
-  Event (fun _ => A) (fun _ => B) (fun i _ _ => i).
+Definition trigger {I} {E : event I I} : E ₑ⇒ itree E :=
+  fun _ q => Vis q (fun r => Ret (Fib r)).
 
-(* WIP
-Definition rec {A B} (body : forall a, itree (ecall A B) (fun _ => B a) t1_0) : forall a, itree evoid (fun _ => B a) t1_0.
-  refine (fun a => _).
-  refine (@mrec T1 (ecall A B) evoid _ t1_0 _ ?>= _).
-  intros [] q.
-  cbn in *.
-  refine (translate (einl) _ _ (body a)).
-  cbn in q.
-  cbn in body.
-  cbn.
-  refine (_ <$> body a).
-  refine (((fun _ => inl) <$> body a)).
-*)
+Definition trigger₀ {E : event₀} (q : qry₀ E) : itree₀ E (rsp₀ E q) := vis₀ q ret₀.
+
+Definition ecall (A : Type) (B : A -> Type) : event₀ := Event₀ A B.
+Definition call {A B} : forall a, itree₀ (ecall A B) (B a) := @trigger₀ (ecall A B).
+Definition rec {A B} (body : forall a, itree₀ (ecall A B) (B a)) : forall a, itree₀ ∅ₑ (B a) :=
+  mrec (fun 't1_0 (a : qry₀ (ecall A B)) => translate inlₑ (B a @ _) _ (body a)) _.
