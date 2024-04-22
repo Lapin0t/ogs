d3aa66f916e17222fe20f9cb022f8440bdcb86e8
Guilhem Jaber <guilhem.jaber@inria.fr>
2024-04-22 14:55:54 +0200

    NamedTerms continued

--- theories/OGS/NamedTerm.v
+++ theories/OGS/NamedTerm.v
@@ -26,7 +26,7 @@ Section withFam.
     }.
 
   Notation "•" := ectx_var.
-  Notation "E @e F" := (ectx_sub E F) (at level 20).
+  Notation "E @ₑ F" := (ectx_sub E F) (at level 20).
 
   Class subst_monoid_ectx_laws `{subst_monoid_ectx} : Prop := 
     {
@@ -34,14 +34,14 @@ Section withFam.
       ectx_sub_var {Γ τ1 τ2} (e : ectx Γ τ1 τ2): ectx_sub e ectx_var = e ;
       ectx_sub_sub {Γ τ1 τ2 τ3 τ4} 
         (E1 : ectx Γ τ1 τ2) (E2 : ectx Γ τ2 τ3) (E3 : ectx Γ τ3 τ4) :
-        E3 @e (E2 @e E1) = (E3 @e E2) @e E1 ;
+        E3 @ₑ (E2 @ₑ E1) = (E3 @ₑ E2) @ₑ E1 ;
     }.
 
   Class subst_module_ectx : Type := {
     esub_val {Γ Δ τ1 τ2} : Γ =[val]> Δ -> ectx Γ τ1 τ2 -> ectx Δ τ1 τ2
   }.
 
-  Notation "u ⊛e E" := (esub_val u E) (at level 30).
+  Notation "u ⊛ₑ E" := (esub_val u E) (at level 30).
 
   Class subst_module_ctx_laxs `{subst_module_ectx} := {
     (* esub_val_proper {Γ Δ} :: Proper (ass_eq Γ Δ ==> forall_relation (fun i => eq ==> eq)) esub_val ; *)
@@ -54,12 +54,12 @@ Section withFam.
     fill {Γ τ1 τ2} : (ectx  Γ τ1 τ2) -> (term Γ τ1) -> (term Γ τ2)
   }.
 
-  Notation "E @t t" := (fill E t) (at level 20).
+  Notation "E @ₜ t" := (fill E t) (at level 20).
   
   Class ectx_module_term_laws `{ectx_module_term} `{subst_monoid_ectx} := {
-    fill_id {Γ τ1} (t : term Γ τ1) : • @t t = t;
+    fill_id {Γ τ1} (t : term Γ τ1) : • @ₜ t = t;
     fill_comp  {Γ τ1  τ2  τ3} (E1 : ectx Γ τ1 τ2) (E2 : ectx Γ τ2 τ3) (t : term Γ τ1) :
-      E2 @t (E1 @t t) = (E2 @e E1) @t t ;
+      E2 @ₜ (E1 @ₜ t) = (E2 @ₑ E1) @ₜ t ;
   }.
 
   Class subst_module_term : Type := {
@@ -77,10 +77,53 @@ Section withFam.
 
   Class coherence_subst_fill_term `{ectx_module_term} `{subst_module_term} `{subst_module_ectx} : Prop := {
     tfill_sub {Γ Δ τ1 τ2} (u : Γ ⇒ᵥ Δ) (E : ectx  Γ τ1 τ2) (t : term  Γ τ1): 
-      u ⊛ₜ (E @t t) = (u ⊛e E) @t (u ⊛ₜ t);
+      u ⊛ₜ (E @ₜ t) = (u ⊛ₑ E) @ₜ (u ⊛ₜ t);
   }.
 
   Class coherence_subst_fill_ectx `{subst_monoid_ectx} `{subst_module_ectx} : Prop := {
     efill_sub {Γ Δ τ1 τ2 τ3} (u : Γ ⇒ᵥ Δ) (E1 : ectx  Γ τ1 τ2) (E2 : ectx  Γ τ2 τ3): 
-      u ⊛e (E2 @e E1) = (u ⊛e E2) @e (u ⊛e E1);
+      u ⊛ₑ (E2 @ₑ E1) = (u ⊛ₑ E2) @ₑ (u ⊛ₑ E1);
   }.
+End withFam.
+
+Variant interactive_type (source_ty : Type) := | ValTy (t:source_ty) | CtxTy (t:source_ty).
+Arguments ValTy {source_ty} t.
+Arguments CtxTy {source_ty} t.
+
+Notation "¬ x" := (CtxTy x) (at level 50).
+
+Equations restrict_valTy {source_ty : Type} : ctx (interactive_type source_ty) -> ctx source_ty :=
+restrict_valTy ∅ := ∅;
+restrict_valTy (Γ ▶ (ValTy τ)) := (restrict_valTy Γ) ▶ τ;
+restrict_valTy (Γ ▶ (CtxTy τ)) := (restrict_valTy Γ).
+
+Definition interactive_baseT (source_ty : baseT) : baseT := {|typ := interactive_type typ|}.
+
+Variant interactive_value {source_ty : baseT} (source_val : @baseV source_ty) (source_ectx : @baseEctx source_ty) : Famₛ (interactive_type typ) :=
+  | IVal {Γ τ} (v : val (restrict_valTy Γ) τ) : interactive_value source_val source_ectx Γ (ValTy τ)
+  | ICtx {Γ τ σ} (α:Γ ∋ ¬σ) (E : ectx (restrict_valTy Γ) τ σ) : interactive_value source_val source_ectx Γ (¬τ).
+
+Program Definition interactive_baseV (source_ty : baseT) (source_val : @baseV source_ty) (source_ectx : @baseEctx source_ty) : @baseV (interactive_baseT source_ty) :=
+{|val := interactive_value source_val source_ectx |}.
+
+Record namedTerm {source_ty : baseT} (interactive_ty : interactive_type typ) (source_term : @baseTerm source_ty) (Γ : ctx (interactive_type typ) ) {σ : typ} := 
+  {α:Γ ∋ ¬σ;  te : term (restrict_valTy Γ) σ}.
+  
+Class isubst_monoid {source_ty : baseT} (source_val : @baseV source_ty) (source_ectx : @baseEctx source_ty) : Type := {
+  iv_var {Γ} : Γ =[interactive_value source_val source_ectx]> Γ ; (* Γ ∋ x -> val Γ x *)
+  iv_sub {Γ Δ} : Γ =[interactive_value source_val source_ectx]> Δ -> interactive_value source_val source_ectx Γ ⇒ᵢ interactive_value source_val source_ectx Δ ;
+}.
+
+Notation "u ⊛ᵢ v" := (iv_sub u _ v) (at level 30).
+Notation "Γ ⇒ᵢᵥ Δ" := (Γ =[interactive_value]> Δ) (at level 30). (*To be corrected *)
+
+(* TODO 
+Class subst_monoid_laws {source_ty : baseT} (source_val : @baseV source_ty) (source_ectx : @baseEctx source_ty) : Prop :=
+{
+  iv_sub_var {Γ1 Γ2} (p : Γ1 =[interactive_value source_val source_ectx]> Γ2) : p ⊛ᵢ iv_var ≡ₐ p ;
+  iv_var_sub {Γ1 Γ2} (p : Γ1 ⇒ᵥ Γ2) : v_var ⊛ p ≡ₐ p ;
+  iv_sub_sub {Γ1 Γ2 Γ3 Γ4} (p : Γ3 ⇒ᵥ Γ4) (q : Γ2 ⇒ᵥ Γ3) (r : Γ1 ⇒ᵥ Γ2) :
+  p ⊛ (q ⊛ r) ≡ₐ (p ⊛ q) ⊛ r ;
+} .
+
+*)
