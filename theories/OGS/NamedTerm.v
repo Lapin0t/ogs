dc916c6a6b185ad3de4a87fdbbda82e5e4b7f389
lapinot <lapinot@sbi.re>
2024-04-29 17:23:49 +0200

    Port NamedTerm and Obs to new context axiomatization.
    Everything is gone! NamedTerms now use direct sum of
    contexts, everything gets easier, but also a bit more
    abstract.

--- theories/OGS/NamedTerm.v
+++ theories/OGS/NamedTerm.v
@@ -25,7 +25,7 @@ Class fill_monoid_laws `{CC : context T C} (ectx : Fam₂ T C) {EM : fill_monoid
 Class subst_module2 `{CC : context T C} (val : Fam₁ T C) (ectx : Fam₂ T C) := {
   e_sub : ectx ⇒₂ ⟦ val , ectx ⟧₂ ;
 }.
-#[global] Arguments e_sub {T C CC val ectx _} [Γ x y] E [Δ] a : rename. 
+#[global] Arguments e_sub {T C CC val ectx _} [Γ x y] E {Δ} a : rename.
 #[global] Notation "E ₑ⊛ a" := (e_sub E a%asgn) (at level 30).
 
 Class subst_module2_laws `{CC : context T C} (val : Fam₁ T C) (ectx : Fam₂ T C)
@@ -35,6 +35,9 @@ Class subst_module2_laws `{CC : context T C} (val : Fam₁ T C) (ectx : Fam₂ T
   e_sub_comp {τ1 τ2 Γ1 Γ2 Γ3} (E : ectx Γ1 τ1 τ2) (u : Γ1 =[val]> Γ2) (v : Γ2 =[val]> Γ3)
     : E ₑ⊛ (u ⊛ v) = (E ₑ⊛ u) ₑ⊛ v ;
 }.
+#[global] Instance e_sub_proper_a `{subst_module2_laws T C val ectx}
+          {Γ1 Γ2 x y} {E : ectx Γ1 x y} : Proper (asgn_eq Γ1 Γ2 ==> eq) (e_sub E).
+Proof. now apply e_sub_proper. Qed.
 
 (* filling a context with a term *)
 Class fill_module `{CC : context T C} (ectx : Fam₂ T C) (term : Fam₁ T C) := {
@@ -53,7 +56,7 @@ Class fill_module_laws `{CC : context T C} (ectx : Fam₂ T C) (term : Fam₁ T 
 Class subst_module1 `{CC : context T C} (val term : Fam₁ T C) := {
   t_sub : term ⇒₁ ⟦ val , term ⟧₁ ;
 }.
-#[global] Arguments t_sub {T C CC val term _} [Γ x] t [Δ] a : rename. 
+#[global] Arguments t_sub {T C CC val term _} [Γ x] t {Δ} a : rename.
 #[global] Notation "t ₜ⊛ a" := (t_sub t a%asgn) (at level 30).
 
 Class subst_module1_laws `{CC : context T C} (val term : Fam₁ T C)
@@ -63,6 +66,9 @@ Class subst_module1_laws `{CC : context T C} (val term : Fam₁ T C)
   t_sub_sub {Γ1 Γ2 Γ3 τ} (t : term Γ1 τ) (u : Γ1 =[val]> Γ2) (v : Γ2 =[val]> Γ3)
     : t ₜ⊛ (u ⊛ v) = (t ₜ⊛ u) ₜ⊛ v ;
 } .
+#[global] Instance t_sub_proper_a `{subst_module1_laws T C val term}
+          {Γ1 Γ2 x} {t : term Γ1 x} : Proper (asgn_eq Γ1 Γ2 ==> eq) (t_sub t).
+Proof. now apply t_sub_proper. Qed.
 
 (* all the laws for a fill-monoid that can also be substituted *)
 Class fill_monoid_subst_module2_coh `{CC : context T C} (val : Fam₁ T C) (ectx : Fam₂ T C)
@@ -70,7 +76,7 @@ Class fill_monoid_subst_module2_coh `{CC : context T C} (val : Fam₁ T C) (ectx
   fsmon_fill_monoid_laws :: fill_monoid_laws ectx ;
   fsmon_subst_module2_laws :: subst_module2_laws val ectx ;
   e_sub_hole {Γ Δ τ} (u : Γ =[val]> Δ) : ∙ ₑ⊛ u = (∙ : ectx _ τ τ) ;
-  e_sub_fill {Γ Δ τ1 τ2 τ3} (E1 : ectx Γ τ1 τ2) (E2 : ectx Γ τ2 τ3) (u : Γ =[val]> Δ) 
+  e_sub_fill {Γ Δ τ1 τ2 τ3} (E1 : ectx Γ τ1 τ2) (E2 : ectx Γ τ2 τ3) (u : Γ =[val]> Δ)
     : (E2 @ₑ E1) ₑ⊛ u = (E2 ₑ⊛ u) @ₑ (E1 ₑ⊛ u);
 }.
 
@@ -101,7 +107,7 @@ Section translation.
   Notation "ᵥ↓ Γ" := (fst Γ) (at level 5).
   Notation "ₖ↓ Γ" := (snd Γ) (at level 5).
 
-  Record f_named (F : C -> T -> Type) (Γ : ictx) := Named {
+  Record f_named (F : Fam₁ T C) (Γ : ictx) := Named {
     n_ty : T ;
     n_kvar : ₖ↓Γ ∋ n_ty ;
     n_elt : F ᵥ↓Γ n_ty ;
@@ -112,142 +118,37 @@ Section translation.
   #[global] Arguments n_elt {F Γ}.
   Notation "⦅ i ⦆ x" := (Named i x) (at level 40).
 
+  Definition iectx : ictx -> T -> Type
+    := fun Γ τ => f_named (fun Δ σ => ectx Δ τ σ) Γ .
+
+  Definition iterm : ictx -> Type
+    := f_named term .
+
   Equations ival : Fam₁ ityp ictx :=
     ival Γ ⌊τ⌋ := val ᵥ↓Γ τ ;
-    ival Γ ¬τ  := f_named (fun Δ σ => ectx Δ τ σ) Γ .
+    ival Γ ¬τ  := iectx Γ τ .
+
+  Definition a_down {Γ Δ} (u : Γ =[ival]> Δ) : ᵥ↓Γ =[val]> ᵥ↓Δ :=
+    fun σ i => u ⌊σ⌋ i.
+  Notation "ₐ↓ u" := (a_down u) (at level 20).
+  Arguments a_down {_ _} _ _ /.
+
+  #[export] Instance a_down_proper {Γ Δ} : Proper (asgn_eq Γ Δ ==> asgn_eq _ _) a_down.
+  Proof. intros ?? Hu ? i; exact (Hu ⌊_⌋ i). Qed.
 
   Equations ival_var {Γ} σ : Γ ∋ σ -> ival Γ σ :=
     ival_var ⌊σ⌋ i := v_var (i : ᵥ↓Γ ∋ _) ;
     ival_var ¬σ  i := ⦅i⦆ ∙ .
 
-  Equations ival_sub {Γ Δ} (γ : Γ =[ival]> Δ) σ (v : ival Γ σ) : ival Δ σ :=
-    ival_sub γ ⌊σ⌋ v       := (v : val _ _) ᵥ⊛ (γ ∘ inl) ;
-    ival_sub γ ¬σ  (⦅i⦆ E) :=
-      let (_ , j, E') := γ (¬ _) i in
-      ⦅j⦆ (E' @ₑ (E ₑ⊛ (γ ∘ inl))) .
+  Equations ival_sub {Γ} σ (v : ival Γ σ) {Δ} (γ : Γ =[ival]> Δ) : ival Δ σ :=
+    ival_sub ⌊σ⌋ v  γ := (v : val _ _) ᵥ⊛ ₐ↓γ ;
+    ival_sub ¬σ  iE γ :=
+      let jF := γ (¬ _) iE.(n_kvar) in
+      ⦅jF.(n_kvar)⦆ (jF.(n_elt) @ₑ (iE.(n_elt) ₑ⊛ ₐ↓γ)) .
 
-  Variant ival (Γ : ictx) : ityp -> Type :=
-  | IVal {τ} (v : val ᵥ↓Γ τ) : ival Γ +τ
-  | ICtx {τ σ} (α : ₖ↓Γ ∋ σ) (E : ectx ᵥ↓Γ τ σ) : ival Γ ¬τ.
-  Derive NoConfusion for ival.
-
-  (* des petits accesseurs pour ival *)
-  Equations ival_v_get_val {Γ σ} (iv : ival Γ +σ) : val ↓Γ σ :=
-    ival_v_get_val (IVal v) := v .
-
-  Equations ival_e_get_ty {Γ σ} (iv : ival Γ ¬σ) : typ :=
-    ival_e_get_ty (@ICtx Γ τ1 τ2 i E) := τ2 .
-
-  Equations ival_e_get_var {Γ σ} (iv : ival Γ ¬σ) : Γ ∋ ¬(ival_e_get_ty iv) :=
-    ival_e_get_var (@ICtx Γ τ1 τ2 i E) := i .
-
-  Equations ival_e_get_ectx {Γ σ} (iv : ival Γ ¬σ) : ectx ↓Γ σ (ival_e_get_ty iv) :=
-    ival_e_get_ectx (@ICtx Γ τ1 τ2 i E) := E .
-
-  (* juste des petits noms plus cohérents avec le reste, aussi les champs c'est bien
-     qu'ils aient des noms explicites vu que ça devient des accesseurs. *)
-(* on embed une variable du sous-context
-
-note: la technique pour faire du équations c'est de commencer par utiliser
-Equations? (avec le ?), qui va te mettre direct en proof mode. Et après de
-raffiner les clauses successivement, genre:
-
-Equations? restrict_emb Γ {σ} (i : ↓Γ ∋ σ) : Γ ∋ +σ :=
-  restrict_emb Γ i := _ .
-
-puis 
-
-Equations? restrict_emb Γ {σ} (i : ↓Γ ∋ σ) : Γ ∋ +σ :=
-  restrict_emb ∅       i := _ ;
-  restrict_emb (Γ ▶ τ) i := _ .
-
-Là on se rend compte que dans le cas 1, il n'y a aucun i possible, du coup soit
-on écrit:
-
-Equations? restrict_emb Γ {σ} (i : ↓Γ ∋ σ) : Γ ∋ +σ :=
-  restrict_emb ∅       (!) ;
-  restrict_emb (Γ ▶ τ) i   := _ .
-
-Soit juste on enlève (normalement il trouve tout seul)
-
-Equations? restrict_emb Γ {σ} (i : ↓Γ ∋ σ) : Γ ∋ +σ :=
-  restrict_emb (Γ ▶ τ) i   := _ .
-
-Puis
-
-Equations? restrict_emb Γ {σ} (i : ↓Γ ∋ σ) : Γ ∋ +σ :=
-  restrict_emb (Γ ▶ +τ) i   := _ ;
-  restrict_emb (Γ ▶ ¬τ) i   := _ .
-
-etc.
-*)
-
-  Equations restrict_emb Γ {σ} (i : ↓Γ ∋ σ) : Γ ∋ +σ :=
-    restrict_emb (Γ ▶ +τ) Ctx.top     := Ctx.top ;
-    restrict_emb (Γ ▶ +τ) (Ctx.pop i) := Ctx.pop (restrict_emb Γ i) ;
-    restrict_emb (Γ ▶ ¬τ) i           := Ctx.pop (restrict_emb Γ i) .
-  #[global] Arguments restrict_emb {Γ σ} i.
-
-  (* plus tard on aura besoin de savoir que c'est injectif *)
-  Lemma restrict_emb_inj {Γ σ} (i j : ↓Γ ∋ σ) : restrict_emb i = restrict_emb j -> i = j.
-  Admitted.
-
-  (* on défini les fibres de restrict_emb *)
-  Variant restrict_view {Γ σ} : Γ ∋ +σ -> Type :=
-  | RestrictV (i : ↓Γ ∋ σ) : restrict_view (restrict_emb i)
-  .
-
-  (* on montre que toute fibre est habitée *)
-  Lemma view_restrict {Γ σ} (i : Γ ∋ +σ) : restrict_view i .
-    clear -i; induction Γ.
-    - dependent elimination i.
-    - dependent elimination i.
-      + exact (@RestrictV (_ ▶ +_) σ Ctx.top).
-      + specialize (IHΓ h).
-        dependent elimination IHΓ.
-        destruct y.
-        exact (@RestrictV (_ ▶ +_) _ (Ctx.pop i)).
-        exact (@RestrictV (_ ▶ ¬_) _ i).
-  Qed.
-
-  (* maintenant pour inverser il suffit de pattern-matcher sur un
-     élément de la fibre *)
-  Equations restrict_get {Γ σ} (i : Γ ∋ +σ) : ↓Γ ∋ σ :=
-    restrict_get i with view_restrict i := {
-       | RestrictV j := j
-    }.
-
-  (* downgrade des assignations (j'ai renommé un changé l'implem pour
-     utiliser les nouveaux outils) *)
-  Definition restrict_ass {Γ Δ} (γ : Γ =[ival]> Δ) : ↓Γ =[val]> ↓Δ :=
-    fun _ i => ival_v_get_val (γ _ (restrict_emb i)) .
-  Notation "↓ₐ u" := (restrict_ass u) (at level 5).
-
-  (* restreindre les assignations préserve l'égalité pointwise *)
-  #[global] Instance restrict_ass_proper {Γ Δ}
-            : Proper (ass_eq Γ Δ ==> ass_eq ↓Γ ↓Δ) restrict_ass .
-  Proof.
-    intros ?? H ? i.
-    unfold restrict_ass; f_equal. 
-    apply H.
-  Qed.
-
-  (* j'ai extrait les defs qui étaient en tactiques inline, pour utiliser equations *)
-  Equations ival_var {Γ} σ : Γ ∋ σ -> ival Γ σ :=
-    ival_var (+ σ) i := IVal (v_var _ (restrict_get i)) ;
-    ival_var (¬ σ) i := ICtx i ∙ .
-
-  Equations ival_sub {Γ Δ} (γ : Γ =[ival]> Δ) {σ} (v : ival Γ σ) : ival Δ σ :=
-    ival_sub γ (IVal v)   := IVal (↓ₐ γ ⊛ᵥ v) ;
-    ival_sub γ (ICtx i E) :=
-      let i' := ival_e_get_var (γ _ i) in
-      let E' := ival_e_get_ectx (γ _ i) in
-      ICtx i' (E' @ₑ (↓ₐ γ ⊛ₑ E)) .
-
-  Definition iterm_sub {Γ Δ} (γ : Γ =[ival]> Δ) (t : iterm Γ) : iterm Δ :=
-    let i' := ival_e_get_var (γ _ t.(itm_var)) in
-    let E' := ival_e_get_ectx (γ _ t.(itm_var)) in
-    ITerm i' (E' @ₜ (↓ₐ γ ⊛ₜ t.(itm_tm))).
+  Definition iterm_sub {Γ} (c : iterm Γ) {Δ} (γ : Γ =[ival]> Δ) : iterm Δ :=
+      let iE := γ (¬ _) c.(n_kvar) in
+      ⦅iE.(n_kvar)⦆ (iE.(n_elt) @ₜ (c.(n_elt) ₜ⊛ ₐ↓γ)) .
 
   #[global] Instance ival_monoid : subst_monoid ival :=
     {| v_var := @ival_var ; v_sub := @ival_sub |}.
@@ -255,22 +156,20 @@ etc.
   #[global] Instance iterm_module : subst_module ival iterm :=
     {| c_sub := @iterm_sub |}.
 
-  (* je te fais les proper.. *)
-  #[global] Instance ival_sub_proper {Γ Δ}
-            : Proper (ass_eq Γ Δ ==> ∀ₕ _, eq ==> eq) ival_sub .
+  #[global] Instance ival_sub_proper
+            : Proper (∀ₕ Γ, ∀ₕ _, eq ==> ∀ₕ Δ, asgn_eq Γ Δ ==> eq) (@ival_sub) .
   Proof.
-    intros ?? Hu ? v ? <-.
-    dependent elimination v; cbn.
-    - now rewrite Hu.
-    - rewrite (Hu _ α); f_equal.
-      now rewrite Hu.
+    intros ? [] ?? <- ??? Hu; cbn.
+    - apply v_sub_proper_a; eauto; now rewrite Hu.
+    - rewrite (Hu (¬_) (n_kvar x)); do 2 f_equal.
+      apply e_sub_proper_a; eauto; now rewrite Hu.
   Qed.
 
-  #[global] Instance iterm_sub_proper {Γ Δ}
-            : Proper (ass_eq Γ Δ ==> eq ==> eq) iterm_sub .
+  #[global] Instance iterm_sub_proper
+            : Proper (∀ₕ Γ, eq ==> ∀ₕ Δ, asgn_eq Γ Δ ==> eq) (@iterm_sub) .
   Proof.
-    intros ?? Hu [ τ α t ] ? <-; unfold iterm_sub; cbn.
-    rewrite (Hu _ α); f_equal.
+    intros ? [ τ α t ] ? <- ??? Hu; unfold iterm_sub; cbn.
+    rewrite (Hu (¬_) α); do 2 f_equal.
     now rewrite Hu.
   Qed.
 
@@ -278,58 +177,10 @@ etc.
      de `subst_monoid_laws ival` et `subst_module_laws ival iterm`. *)
   Lemma ival_sub_var {Γ1 Γ2} (p : Γ1 =[ival]> Γ2) : p ⊛ ival_var ≡ₐ p .
   Proof.
-    intros a i.
-    destruct a; cbn.
-    - (* ici c'est tricky mais la manière dont j'ai fais les loi des subst_monoid fais
-         que c'est un peu chiant de les appliquer, souvent il faut faire des `change`
-         (remplace un terme par un autre qui est convertible). Là globalement on est
-         en train de substituter une variable: `foo ⊛ᵥ v_var _ i`, sauf que la loi
-         qu'on veut appliquer parle de `foo ⊛ v_var`. *)
-      change (?u ⊛ᵥ v_var _ ?i) with ((u ⊛ v_var) _ i).
-      rewrite v_sub_var.
-      clear.
-      (* là grosso modo fini la preuve, mais il reste à gérer les restrict..
-         ici on veut montrer que inverser une variable de valeur puis aller
-         chercher dans l'assignation restreinte, c'est la même chose qu'aller
-         directement chercher dans l'assignation initiale. Grosso modo restrict_get
-         c'est l'inversion de la variable, alors que la restriction d'assignation fait
-         l'inverse (elle prend une variable simple et l'embed dans le contexte etendu
-         pour recupérer la valeur). C'est maintenant que ça paye la view: on va unfold
-         un peu pour faire apparaitre le moment où restrict_get pattern-match sur
-         l'élément de la fibre donné par `view_restrict` et on va aussi pattern
-         matcher dessus dans la preuve. Ce truc va pop souvent donc il faudrait
-         l'extraire dans un lemme.
-      *)
-      unfold restrict_get.
-      destruct (view_restrict i); cbn.
-      (* et là c'est toujours pas fini, il y a le IVal qui embète: déjà on un unfold
-         ce ↓ₐ pour y voir plus clair *)
-      unfold restrict_ass.
-      (* donc là c'est juste notre accesseur: on doit monter que
-         `IVal (ival_v_get_val v) = v`. Encore une fois il faudrait l'extraire, ça va
-         probablement réapparaitre. On commence par abstraire `v`, aka ici
-         `p _ (restrict_emb i)` *)
-      remember (p _ (restrict_emb i)) as v; clear.
-      now dependent elimination v.
-    - (* là on voit que la seule manière dont on utilise p et i c'est pour
-         construire `p _ i`, donc dans le doute on abstrait *)
-      remember (p _ i) as v; clear i Heqv.
-      dependent elimination v; cbn.
-      rewrite e_sub_hole. (* hop, une petite loi de cohérence qu'on avait oublié!! *)
-      rewrite e_fill_hole.
-      reflexivity.
+    intros [] i; cbn.
+    - change (ₐ↓ ival_var) with (a_id (Γ:=ᵥ↓Γ2)).
+      now rewrite v_var_sub.
+    - change (ₐ↓ ival_var) with (a_id (Γ:=ᵥ↓Γ2)).
+      now rewrite e_hole_fill, e_sub_id.
   Qed.
-
-  (* les lemmes qu'on a prouvé inline au-dessus, mais qui vont etre pratiques *)
-  Lemma ival_v_get_id {Γ σ} (v : ival Γ +σ) : IVal (ival_v_get_val v) = v. 
-  Admitted.
-
-  (* bonus, j'imagine qu'on en aura besoin tout pareil *)
-  Lemma ival_e_get_id {Γ σ} (v : ival Γ ¬σ)
-        : ICtx (ival_e_get_var v) (ival_e_get_ectx v) = v. 
-  Admitted.
-
-  Lemma ass_restrict_get {Γ1 Γ2 σ} (u : Γ1 =[ival]> Γ2) (i : Γ1 ∋ +σ)
-        : IVal (↓ₐ u _ (restrict_get i)) = u _ i .
-  Admitted.
 End translation.
--- theories/OGS/Obs.v
+++ theories/OGS/Obs.v
@@ -1,10 +1,8 @@
-From Coinduction Require Import coinduction tactics.
-
 From OGS Require Import Prelude.
 From OGS.Utils Require Import Rel.
 From OGS.Ctx Require Import All.
 From OGS.OGS Require Import Subst.
-From OGS.ITree Require Import Event ITree Delay Eq.
+From OGS.ITree Require Import Event ITree Delay Eq Properties.
 
 Open Scope ctx_scope.
 Reserved Notation "O ∙" (at level 5).
@@ -26,43 +24,38 @@ of the language:
 - a map [dom] mapping observations to values filling their copattern
 
 |*)
-#[global] Notation obs_struct T := (f_slice T).
-#[global] Notation dom := s_dom.
-#[global] Coercion s_op : obs_struct >-> Funclass.
-
-Definition pointed_obs {T} (O : obs_struct T) : Fam T := var ∥ ⦇ O ⦈.
+#[global] Notation obs_struct T C := (Oper T C).
+#[global] Notation dom := o_dom.
+Definition pointed_obs `{CC : context T C} (O : Oper T C) : Fam₀ T C
+  := c_var ∥ ⦉O⦊.
 #[global] Notation "O ∙" := (pointed_obs O).
-Definition nf {T} (O : obs_struct T) (X : Fam₁ T) : Fam T := var ∥ (O # X).
+Definition nf `{CC : context T C} (O : obs_struct T C) (X : Fam₁ T C) : Fam₀ T C
+  := c_var ∥ (O # X).
 
-Definition mk_nf {T} {O : obs_struct T} {X : Fam₁ T} {Γ t}
-           (i : t ∈ Γ) (o : O t) (a : dom o =[X]> Γ)
-  : nf O X Γ
-  := ⟨ i | OFill o a ⟩.
-#[global] Notation "i ⋅ o ⦉ a ⦊" := (mk_nf i o a%asgn).
-
-Definition nf_to_obs {T O} {X : Fam₁ T} : forall Γ, nf O X Γ -> O∙ Γ
+Definition nf_to_obs `{CC : context T C} {O} {X : Fam₁ T C} : forall Γ, nf O X Γ -> O∙ Γ
   := f_cut_map f_id₁ forget_args.
 #[global] Coercion nf_to_obs : nf >-> pointed_obs.
 
-Definition then_to_obs {T O} {X : Fam₁ T} {Γ} : delay (nf O X Γ) -> delay (O∙ Γ)
+Definition then_to_obs `{CC : context T C} {O} {X : Fam₁ T C} {Γ}
+  : delay (nf O X Γ) -> delay (O∙ Γ)
   := fmap_delay (nf_to_obs Γ).
 
 Section with_param.
-  Context {T : Type} {O : obs_struct T} {X : Fam₁ T}.
+  Context `{CC : context T C} {O : obs_struct T C} {X : Fam₁ T C}.
 
   Definition nf_ty {Γ} (n : nf O X Γ) : T
     := n.(cut_ty).
-  Definition nf_var {Γ} (n : nf O X Γ) : nf_ty n ∈ Γ
+  Definition nf_var {Γ} (n : nf O X Γ) : Γ ∋ nf_ty n
     := n.(cut_l).
   Definition nf_obs {Γ} (n : nf O X Γ) : O (nf_ty n)
     := n.(cut_r).(fill_op).
-  Definition nf_dom {Γ} (n : nf O X Γ) : ctx T
+  Definition nf_dom {Γ} (n : nf O X Γ) : C
     := dom n.(cut_r).(fill_op).
   Definition nf_args {Γ} (n : nf O X Γ) : nf_dom n =[X]> Γ
     := n.(cut_r).(fill_args).
 
   Variant nf_eq {Γ} : nf O X Γ -> nf O X Γ -> Prop :=
-  | NfEq {t} {i : t ∈ Γ} {o a1 a2} : a1 ≡ₐ a2 -> nf_eq (i⋅o⦉a1⦊) (i⋅o⦉a2⦊).
+  | NfEq {t} {i : Γ ∋ t} {o a1 a2} : a1 ≡ₐ a2 -> nf_eq ⟨i|o⦇a1⦈⟩ ⟨i|o⦇a2⦈⟩.
 
   #[global] Instance nf_eq_Equivalence {Γ} : Equivalence (@nf_eq Γ).
   Proof.
@@ -79,13 +72,9 @@ Section with_param.
   #[global] Instance then_to_obs_proper {Γ}
     : Proper (@comp_eq Γ ==> it_eq (eqᵢ _) (i:=_)) then_to_obs.
   Proof.
-    intros a b; revert a b; unfold it_eq; coinduction R CIH; intros a b H.
-    unfold comp_eq in H; apply it_eq_step in H.
-    cbn in *; unfold observe in H.
-    destruct (_observe a), (_observe b); dependent elimination H; econstructor.
-    - cbn in r_rel; now destruct r_rel.
-    - now apply CIH.
-    - destruct q1.
+    intros a b; unfold comp_eq, then_to_obs, fmap_delay; cbn; intros H.
+    eapply fmap_eq; [ | exact H ].
+    now intros [] ?? [].
   Qed.
 End with_param.
 
